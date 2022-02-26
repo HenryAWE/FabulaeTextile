933e50afad736885709d45aa91128439b0243937
AWE Henry <mouth11or12@outlook.com>
2022-02-26 15:40:56 +0800

    Update vector

--- lib/include/fabtex/vector.hpp
+++ lib/include/fabtex/vector.hpp
@@ -12,6 +12,7 @@ namespace fabtex
     template <typename T>
     class vec<2, T>
     {
+        static_assert(std::is_arithmetic_v<T>);
     public:
         typedef T value_type;
         typedef std::size_t size_type;
@@ -22,13 +23,13 @@ namespace fabtex
         typedef pointer iterator;
         typedef const_pointer const_iterator;
 
-        vec() : vec(static_cast<value_type>(0)) {}
+        constexpr vec() : vec(static_cast<value_type>(0)) {}
         vec(const vec&) = default;
         vec(vec&&) = default;
         template <typename U>
-        vec(const vec<2, U>& v) : vec(value_type(v[0]), value_type(v[1])) {}
-        explicit vec(const_reference v) : m_data{ v, v } {}
-        vec(const_reference v0, const_reference v1)
+        constexpr vec(const vec<2, U>& v) : vec(value_type(v[0]), value_type(v[1])) {}
+        constexpr explicit vec(const_reference v) : m_data{ v, v } {}
+        constexpr vec(const_reference v0, const_reference v1)
             : m_data{ v0, v1 } {}
 
         vec& operator=(const vec&) = default;
@@ -37,43 +38,43 @@ namespace fabtex
         static constexpr size_type dimension = 2;
         static constexpr size_type size() noexcept { return dimension; }
 
-        reference operator[](size_type idx) noexcept
+        constexpr reference operator[](size_type idx) noexcept
         {
             return m_data[idx];
         }
-        const_reference operator[](size_type idx) const noexcept
+        constexpr const_reference operator[](size_type idx) const noexcept
         {
             return m_data[idx];
         }
 
-        friend vec operator+(const vec& lhs, const vec& rhs)
+        constexpr friend vec operator+(const vec& lhs, const vec& rhs)
         {
             return vec(
                 lhs[0] + rhs[0],
                 lhs[1] + rhs[1]
             );
         }
-        friend vec operator+(const vec& lhs, const_reference rhs) { return lhs + vec(rhs); }
-        friend vec operator+(const_reference lhs, const vec& rhs) { return vec(lhs) + rhs; }
+        constexpr friend vec operator+(const vec& lhs, const_reference rhs) { return lhs + vec(rhs); }
+        constexpr friend vec operator+(const_reference lhs, const vec& rhs) { return vec(lhs) + rhs; }
 
-        friend vec operator-(const vec& lhs, const vec& rhs)
+        constexpr friend vec operator-(const vec& lhs, const vec& rhs)
         {
             return vec(
                 lhs[0] - rhs[0],
                 lhs[1] - rhs[1]
             );
         }
-        friend vec operator-(const vec& lhs, const_reference rhs) { return lhs - vec(rhs); }
-        friend vec operator-(const_reference lhs, const vec& rhs) { return vec(lhs) - rhs; }
+        constexpr friend vec operator-(const vec& lhs, const_reference rhs) { return lhs - vec(rhs); }
+        constexpr friend vec operator-(const_reference lhs, const vec& rhs) { return vec(lhs) - rhs; }
 
-        friend vec operator*(const vec& lhs, const_reference rhs)
+        constexpr friend vec operator*(const vec& lhs, const_reference rhs)
         {
             return vec(
                 lhs[0] * rhs,
                 lhs[1] * rhs
             );
         }
-        friend vec operator*(const_reference lhs, const vec& rhs)
+        constexpr friend vec operator*(const_reference lhs, const vec& rhs)
         {
             return vec(
                 lhs * rhs[0],
@@ -81,7 +82,7 @@ namespace fabtex
             );
         }
 
-        friend vec operator/(const vec& lhs, const_reference rhs)
+        constexpr friend vec operator/(const vec& lhs, const_reference rhs)
         {
             return vec(
                 lhs[0] / rhs,
@@ -89,7 +90,7 @@ namespace fabtex
             );
         }
 
-        vec operator-() const
+        constexpr vec operator-() const
         {
             return vec(
                 -m_data[0],
@@ -97,17 +98,17 @@ namespace fabtex
             );
         }
 
-        bool operator==(const vec& rhs) const = default;
+        constexpr bool operator==(const vec& rhs) const = default;
 
         constexpr pointer data() noexcept { return m_data; }
         constexpr const_pointer data() const noexcept { return m_data; }
 
-        iterator begin() noexcept { return data(); }
-        iterator end() noexcept { return data() + dimension; }
-        const_iterator begin() const noexcept { return data(); }
-        const_iterator end() const noexcept { return data() + dimension; }
-        const_iterator cbegin() const noexcept { return data(); }
-        const_iterator cend() const noexcept { return data() + dimension; }
+        constexpr iterator begin() noexcept { return data(); }
+        constexpr iterator end() noexcept { return data() + dimension; }
+        constexpr const_iterator begin() const noexcept { return data(); }
+        constexpr const_iterator end() const noexcept { return data() + dimension; }
+        constexpr const_iterator cbegin() const noexcept { return data(); }
+        constexpr const_iterator cend() const noexcept { return data() + dimension; }
 
     private:
         T m_data[dimension];
