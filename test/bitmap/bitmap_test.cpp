f43cad39410ba55f8b5d5a72a993b04936d503e3
AWE Henry <mouth11or12@outlook.com>
2022-02-27 16:25:59 +0800

    Update bitmap

--- test/bitmap/bitmap_test.cpp
+++ test/bitmap/bitmap_test.cpp
@@ -44,6 +44,44 @@ TEST(TestBitmap, BasicFunctions)
     }
     img.index({ 2, 2 }) = { 3, 3, 3, 3 };
     EXPECT_EQ(img.index({2, 2}), (std::array<std::uint8_t, 4>{ 3, 3, 3, 3 }));
+
+    auto img2 = img;
+    EXPECT_EQ(img2, img);
+}
+
+TEST(TestBitmap, TestView)
+{
+    bitmap_rgba8888 img;
+    img.resize({ 4, 4 });
+    auto bv = img.subview({ 1, 1 }, { 2, 2 });
+    EXPECT_EQ(&bv.bitmap(), &img);
+    EXPECT_EQ(bv.size(), (vec<2, std::size_t>(2)));
+    bv.fill({ 1, 1, 1, 1 });
+    for(std::size_t i : { 1, 2 })
+    {
+        for(auto j : img.index({ i, i }))
+        {
+            EXPECT_EQ(j, 1);
+        }
+    }
+    for(std::size_t i : { 0, 1 })
+    {
+        for(auto j : bv.index({ i, i }))
+        {
+            EXPECT_EQ(j, 1);
+        }
+    }
+
+    bitmap_rgba8888 img2;
+    img2.resize({ 1, 2 }, { 2, 2, 2, 2 });
+    bv.copy(img2);
+    for(std::size_t i : { 1, 2 })
+    {
+        for(auto j : img.index({ 1, i }))
+        {
+            EXPECT_EQ(j, 2);
+        }
+    }
 }
 
 int main(int argc, char* argv[])
